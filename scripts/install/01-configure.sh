82c8c39ea16e6a315dea2e055d2b82a1af599cc2
eoli3n <jonathan.kirszling@runbox.com>
2021-06-06 18:52:44 +0200

    dynamic ashift in script

--- scripts/install/01-configure.sh
+++ scripts/install/01-configure.sh
@@ -60,9 +60,20 @@ read -r -p "ZFS passphrase: " pass
 echo "$pass" > /etc/zfs/zroot.key
 chmod 000 /etc/zfs/zroot.key
 
+# Check sector size
+fdisk -l "$DISK"
+print "check Sector size Sector size (logical/physical)"
+echo "If 512 bytes / 512 bytes, set ashift to 9 else if 512 bytes / 4096 bytes, set ashift to 12"
+select ENTRY in 9 12;
+do
+    ashift="$ENTRY"
+    echo "Set ashift to $ENTRY."
+    break
+done
+
 # Create ZFS pool
 print "Create ZFS pool"
-zpool create -f -o ashift=12                          \
+zpool create -f -o ashift="$ashift"                   \
              -o autotrim=on                           \
              -O acltype=posixacl                      \
              -O compression=zstd                      \
